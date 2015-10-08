b80a8e1b4d6fea5655258a5a0dd9033ccab46277
julianlinux <julianlinux@julianlinux-X555LA>
2015-10-08 02:03:27 +0200

    k

---
+++ src/bol28/Bol28.java
@@ -0,0 +1,32 @@
+
+package bol28;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author julianlinux
+ */
+public class Bol28 {
+
+    public static void main(String[] args) {
+        float centigrados,kelvin,farenheit;
+        Scanner dato=new Scanner(System.in);
+        System.out.println("Introduce los grados centigrados:");
+        centigrados=dato.nextFloat();
+        kelvin=centigrados+273;
+        farenheit= (float) (centigrados*1.8+32);
+        System.out.println("kelvin:"+kelvin);
+        System.out.println("farenheit:"+farenheit);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
